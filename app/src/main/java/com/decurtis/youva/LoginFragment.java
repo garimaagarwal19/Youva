13c161fdb4de12e6934050a8cf7455e9f9207564
Garima.Chamaria <garima.chamaria@decurtis.com>
2018-12-20 23:01:52 +0530

    Added Login fragment

---
+++ app/src/main/java/com/decurtis/youva/LoginFragment.java
@@ -0,0 +1,20 @@
+package com.decurtis.youva;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Garima Chamaria on 20/12/18.
+ */
+public class LoginFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.google_login, container, false);
+    }
+}
--- app/src/main/java/com/decurtis/youva/MainActivity.java
+++ app/src/main/java/com/decurtis/youva/MainActivity.java
@@ -2,12 +2,23 @@ package com.decurtis.youva;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout mFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFrameLayout = findViewById(R.id.frame_container);
+
+        addLoginFragment();
+    }
+
+    private void addLoginFragment() {
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.frame_container, new LoginFragment()).commit();
     }
 }
