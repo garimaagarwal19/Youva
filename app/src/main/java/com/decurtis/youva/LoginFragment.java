29c234b5f8432afd0f29cdda0878249574b4deae
Garima.Chamaria <garima.chamaria@decurtis.com>
2018-12-21 18:23:10 +0530

    Added google sign in functionality

--- app/src/main/java/com/decurtis/youva/LoginFragment.java
+++ app/src/main/java/com/decurtis/youva/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.decurtis.youva;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,13 +9,83 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+
 /**
  * Created by Garima Chamaria on 20/12/18.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+    private View mView;
+    private SignInButton mSignInButton;
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
+
+    public static final int REQ_CODE = 9001;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.google_login, container, false);
+        mView = inflater.inflate(R.layout.google_login, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mSignInButton = (SignInButton) mView.findViewById(R.id.google_button);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performGoogleSignIn();
+            }
+        });
+    }
+
+    private void performGoogleSignIn() {
+        String clientId = getActivity().getResources().getString(R.string.client_id);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(clientId)
+                .requestServerAuthCode(clientId, false)
+                .requestEmail()
+                .requestScopes(new Scope(Scopes.PROFILE))
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), signInOptions);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
+                .build();
+        mGoogleApiClient.connect();
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        getActivity().startActivityForResult(signInIntent, REQ_CODE);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
